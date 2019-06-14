f471660fdc70260c7ea04305564f46f9b0fd334c
rishulike <rishabhjain2553@gmail.com>
2019-06-14 10:28:02 +0530

    i am don

---
+++ random match using for loop.py
@@ -0,0 +1,8 @@
+print("Welcome to My world")
+a=str(input())
+print("welcome to visual form",a)
+print("here you have 3 chance to  exit from my world")
+print("you have need enter 3 values if your values matched within random value than you got exit otherwise you never got exit")
+print("kyunki baby Ram nhi Ravan hoo mai")
+import random as ran
+b=ran. randint(0,100)
