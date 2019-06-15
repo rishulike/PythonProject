67b9f4fe5203f11a08593d68847a092d23ccd1a8
rishulike <rishabhjain2553@gmail.com>
2019-06-15 19:40:04 +0530

    Home Work

--- random match using for loop.py
+++ random match using for loop.py
@@ -6,3 +6,13 @@ print("you have need enter 3 values if your values matched within random value t
 print("kyunki baby Ram nhi Ravan hoo mai")
 import random as ran
 b=ran. randint(0,100)
+for i in range(0,3):
+    i=int(input())
+if i<b:
+    print("number is small")
+elif i>b:
+    print("number is greator")
+else:
+    print("number is matched")
+print(b)
+
